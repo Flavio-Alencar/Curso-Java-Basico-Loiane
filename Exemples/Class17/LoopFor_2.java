49d573715daca47ea64122f6044331f6365da6e3
Flávio Alencar <f.s.alencar1@gmail.com>
2024-09-09 11:16:07 -0300

    Add files via upload

---
+++ Exemples/Class17/LoopFor_2.java
@@ -0,0 +1,22 @@
+//Example 03 - Class 17
+//Loiane Training - Java Basic Course
+//Edited by Flávio Alencar
+//Date: 09SET24 - 10h50 AM
+
+
+public class LoopFor_2
+{
+    public static void main(String[] args)
+    {
+        //Variable
+        //----------------------------------------------------------------------
+        int count = 0;          //Variable Counter.
+        //----------------------------------------------------------------------
+        
+        for (; count < 10;)
+        {
+            System.out.println("Value of Count: " +count);
+            count +=2;
+        }
+    }
+}
---
+++ Exemples/Class17/LoopFor_3.java
@@ -0,0 +1,20 @@
+//Example 04 - Class 17
+//Loiane Training - Java Basic Course
+//Edited by Flávio Alencar
+//Date: 09SET24 - 10h50 AM
+
+
+public class LoopFor_3
+{
+    public static void main(String[] args)
+    {
+        //Variable
+        //----------------------------------------------------------------------
+        int soma = 0;          //Variable of Sum.
+        //----------------------------------------------------------------------
+        
+        for (int i = 1; i < 5; soma += i++);
+        System.out.println("Value of Sun: " +soma);
+        
+    }
+}
