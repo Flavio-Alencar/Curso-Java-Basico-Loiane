3ee6ee44aa210fef02126c5a9a0e877e09083dc8
Flávio Alencar <f.s.alencar1@gmail.com>
2024-09-29 10:35:48 -0300

    Add files via upload

---
+++ Exemplos/Aula 10/Variaveis.java
@@ -0,0 +1,19 @@
+//Aula 10 - Declaração de Variáveis
+
+public class Variaveis {
+
+    public static void main(String[] args) {
+        
+        int idade = 43;
+        String nome = "Flavio";
+        String sobrenome = "Alencar";
+        
+        System.out.println("Nome: " +nome);
+        System.out.println("Sobrenome: " +sobrenome);
+        System.out.println("Idade: " +idade);
+        
+        
+        
+        
+    }
+}
