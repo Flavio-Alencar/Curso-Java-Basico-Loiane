e335db86039a21ce55d6c19c2b4e97f6bd70ae97
Flávio Alencar <f.s.alencar1@gmail.com>
2024-09-29 10:42:47 -0300

    Add files via upload

---
+++ Exemplos/Aula 17/LoopFor_1.java
@@ -0,0 +1,16 @@
+//Example 02 - Class 17
+//Loiane Training - Java Basic Course
+//Edited by Flávio Alencar
+//Date: 09SET24 - 10h41 AM
+
+
+public class LoopFor_1
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0, j = 10; i < j; i++, j--)
+        {
+            System.out.println("i = " +i +"; j = " +j);            
+        }
+    }
+}
