1c696d406a5108310ff64c17496662d78ca3e99d
Flávio Alencar <f.s.alencar1@gmail.com>
2024-11-02 18:26:59 -0300

    Add files via upload

---
+++ Exemplos/Aula 36/Endereco.java
@@ -0,0 +1,70 @@
+//Exemplo 01 - Aula 35
+//Loiane Training - Java Basic Course
+//Editado por Flávio Alencar
+//Data: 02NOV24 - 15h39
+//Assunto: Relacionamento tem um
+
+package exemplos.Aula36;
+
+public class Endereco {
+    //                      DECLARAÇÃO DE VARIAVEIS GLOBAIS
+    //--------------------------------------------------------------------------
+    private  String endereco;
+    private  String numero;
+    private  String bairro;
+    private  String cidade;
+    private  String estado;
+    private  String cep;
+    //--------------------------------------------------------------------------
+
+    public  String getEndereco() {
+        return endereco;
+    }
+
+    public  void setEndereco(String aEndereco) {
+        endereco = aEndereco;
+    }
+
+    public  String getNumero() {
+        return numero;
+    }
+
+    public  void setNumero(String aNumero) {
+        numero = aNumero;
+    }
+
+    public  String getBairro() {
+        return bairro;
+    }
+
+    public  void setBairro(String aBairro) {
+        bairro = aBairro;
+    }
+
+    public  String getCidade() {
+        return cidade;
+    }
+
+    public  void setCidade(String aCidade) {
+        cidade = aCidade;
+    }
+
+    public  String getEstado() {
+        return estado;
+    }
+
+    public  void setEstado(String aEstado) {
+        estado = aEstado;
+    }
+
+    public  String getCep() {
+        return cep;
+    }
+
+    public  void setCep(String aCep) {
+        cep = aCep;
+    }
+    
+    
+    
+}
---
+++ Exemplos/Aula 36/Telefone.java
@@ -0,0 +1,43 @@
+//Exemplo 01 - Aula 35
+//Loiane Training - Java Basic Course
+//Editado por Flávio Alencar
+//Data: 02NOV24 - 15h39
+//Assunto: Relacionamento tem um
+
+package exemplos.Aula36;
+
+public class Telefone {
+        //                      DECLARAÇÃO DE VARIAVEIS GLOBAIS
+    //--------------------------------------------------------------------------
+    private  String tipo;
+    private  String ddd;
+    private  String numero;
+    //--------------------------------------------------------------------------
+
+    public  String getTipo() {
+        return tipo;
+    }
+
+    public  void setTipo(String aTipo) {
+        tipo = aTipo;
+    }
+
+    public  String getDdd() {
+        return ddd;
+    }
+
+    public  void setDdd(String aDdd) {
+        ddd = aDdd;
+    }
+
+    public  String getNumero() {
+        return numero;
+    }
+
+    public  void setNumero(String aNumero) {
+        numero = aNumero;
+    }
+    
+    
+    
+}
