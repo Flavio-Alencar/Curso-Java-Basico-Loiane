df54d855126c4ef9fb1189054fc1921591a6dd9d
Flávio Alencar <f.s.alencar1@gmail.com>
2024-11-06 14:57:47 -0300

    Add files via upload

---
+++ Exemplos/Aula 38/Pessoa.java
@@ -0,0 +1,52 @@
+//Exemplo 01 - Aula 38
+//Loiane Training - Java Basic Course
+//Editado por Flávio Alencar
+//Data: 06NOV24 - 14h12
+//Assunto: Herança super
+
+package exemplos.Aula38;
+
+public class Pessoa {
+    //                      DECLARAÇÃO DE VARIAVEIS GLOBAIS
+    //--------------------------------------------------------------------------
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String cpf;
+    //--------------------------------------------------------------------------
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+    
+}
