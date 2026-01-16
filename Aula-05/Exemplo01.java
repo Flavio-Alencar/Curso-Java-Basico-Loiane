7176df499c0eda76ae8be4a9af461e6c8716890b
Flávio Alencar <f.s.alencar1@gmail.com>
2026-01-16 16:23:06 -0300

    Add files via upload

---
+++ Aula-05/Exemplo01.java
@@ -0,0 +1,12 @@
+//Exemplo 01 - Aula 05
+//Flavio Silvestre de Alencar - 16JAN26 - 16h01
+//Imprimir "Hellow World"
+//------------------------------------------------------------------------------
+
+public class Exemplo01 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
