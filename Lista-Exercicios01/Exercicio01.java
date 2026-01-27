df72a1aa21085fcfcd55a29e0e6fc7d94bce24e5
Flávio Alencar <f.s.alencar1@gmail.com>
2026-01-27 09:50:56 -0300

    Add files via upload

---
+++ Lista-Exercicios01/Exercicio01.java
@@ -0,0 +1,16 @@
+package ListaExercicio01;
+
+//Exercicio 01 - Aula 05
+//Flavio Silvestre de Alencar - 26JAN26 - 21h51
+//Descrição: Realizar um Programa qpara Imprimir 'Olá Mundo'"
+//------------------------------------------------------------------------------
+
+public class Exercicio01 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println(" ");
+        System.out.println("Olá Mundo!!!");
+        System.out.println(" ");
+    }
+}
