061e8c2729c2eedf58577cbc1aff9c7862d41f2c
Flávio Alencar <f.s.alencar1@gmail.com>
2026-02-17 20:54:32 -0300

    Add files via upload

---
+++ Aula-06/Exemplo01.java
@@ -0,0 +1,15 @@
+package Aula06;
+
+//Exercicio 01 - Aula 06
+//Flavio Silvestre de Alencar - 17FEV26 - 19h00
+//Descrição: Realizar um Programa com passagem de argumento.
+//------------------------------------------------------------------------------
+
+public class Exemplo01 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Você digitou: " + args[0]);        
+    }
+        
+}
