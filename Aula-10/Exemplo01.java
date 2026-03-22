633a0e54b6975009500b266e9cf20a6f71c9687c
Flávio Alencar <f.s.alencar1@gmail.com>
2026-03-22 11:36:29 -0300

    Add files via upload

---
+++ Aula-10/Exemplo01.java
@@ -0,0 +1,23 @@
+package Aula10;
+
+//Exercicio 01 - Aula 10
+//Flavio Silvestre de Alencar - 08MAR26 - 19h13
+//Descrição: Declaração de Variáveis
+//------------------------------------------------------------------------------
+
+public class Exemplo01 {
+    
+    public static void main(String[] args) {
+        
+        //Declaração de Varáveis
+        //----------------------------------------------------------------------
+        int idade;
+        String nome;
+        //----------------------------------------------------------------------
+        
+        System.out.printf("Nome: %s%n", "Flávio Alencar");
+        System.out.printf("Idade: %d%n", 45);       
+        
+    }
+    
+}
